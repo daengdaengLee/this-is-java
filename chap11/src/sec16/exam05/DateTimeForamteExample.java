b34c89b2429d739b45363ffd7049b8acdfe9e361
daengdaengLee <gunho1020@gmail.com>
2019-04-22 22:34:15 +0900

    ENH: Chapter 11 Section 16 Exam 05

---
+++ chap11/src/sec16/exam05/DateTimeForamteExample.java
@@ -0,0 +1,13 @@
+package sec16.exam05;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeForamteExample {
+  public static void main(String[] args) {
+    LocalDateTime now = LocalDateTime.now();
+    DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 M월 d일 a h시 m분");
+    String nowString = now.format(dateTimeFormatter);
+    System.out.println(nowString);
+  }
+}
