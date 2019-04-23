5da49a7f5ebc20fad8e8f8de205ccc951017552e
daengdaengLee <gunho1020@gmail.com>
2019-04-23 21:40:33 +0900

    ENH: Chapter 12 Section 05

---
+++ chap12/src/sec05/TargetThread.java
@@ -0,0 +1,33 @@
+package sec05;
+
+public class TargetThread extends Thread {
+  @Override
+  public void run() {
+    for (long i = 0; i < 1000000000; i++) {
+    }
+    for (long i = 0; i < 1000000000; i++) {
+    }
+    for (long i = 0; i < 1000000000; i++) {
+    }
+    for (long i = 0; i < 1000000000; i++) {
+    }
+    for (long i = 0; i < 1000000000; i++) {
+    }
+
+    try {
+      Thread.sleep(1500);
+    } catch (Exception e) {
+    }
+
+    for (long i = 0; i < 1000000000; i++) {
+    }
+    for (long i = 0; i < 1000000000; i++) {
+    }
+    for (long i = 0; i < 1000000000; i++) {
+    }
+    for (long i = 0; i < 1000000000; i++) {
+    }
+    for (long i = 0; i < 1000000000; i++) {
+    }
+  }
+}
