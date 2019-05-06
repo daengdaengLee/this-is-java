c865c68043aaf366566a9be31df9b94d91d8611c
daengdaengLee <gunho1020@gmail.com>
2019-05-06 11:59:05 +0900

    ENH: Chapter 13 Section 07

---
+++ chap13/src/sec07/ChildProduct.java
@@ -0,0 +1,18 @@
+package sec07;
+
+public class ChildProduct<T, M, C> extends Product<T, M> {
+  private C company;
+
+  public C getCompany() {
+    return company;
+  }
+
+  public void setCompany(C company) {
+    this.company = company;
+  }
+
+  @Override
+  public String toString() {
+    return "[" + this.getKind() + "][" + this.getModel() + "][" + this.getCompany() + "]";
+  }
+}
