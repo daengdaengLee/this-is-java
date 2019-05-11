96daf16b3a2fc23b80fb1d01b1934830a31e2161
daengdaengLee <gunho1020@gmail.com>
2019-05-11 18:24:55 +0900

    ENH: Chapter 14 Section 05

---
+++ chap14/src/sec05/RunnableExample.java
@@ -0,0 +1,21 @@
+package sec05;
+
+public class RunnableExample {
+  public static void main(String[] args) {
+    Runnable runnable = () -> {
+      for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+      }
+    };
+
+    Thread thread1 = new Thread(runnable);
+    thread1.start();
+
+    Thread thread2 = new Thread(() -> {
+      for (int i = 0; i < 10; i++) {
+        System.out.println(i);
+      }
+    });
+    thread2.start();
+  }
+}
