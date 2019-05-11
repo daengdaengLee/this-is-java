99b8a26d2b41a5b4cb78e613af6a74d3a6676940
daengdaengLee <gunho1020@gmail.com>
2019-05-11 21:12:51 +0900

    ENH: Chapter 14 Section 05 Exam 06

---
+++ chap14/src/sec05/exam06/ConsumerAndThenExample.java
@@ -0,0 +1,14 @@
+package sec05.exam06;
+
+import java.util.function.Consumer;
+
+public class ConsumerAndThenExample {
+  public static void main(String[] args) {
+    Consumer<Member> consumerA = m -> System.out.println("consumerA : " + m.getName());
+
+    Consumer<Member> consumerB = m -> System.out.println("consumerB : " + m.getName());
+
+    Consumer<Member> consumerAB = consumerA.andThen(consumerB);
+    consumerAB.accept(new Member("홍길동", "hong", null));
+  }
+}
