9baec8ec0a1cb382298c65720b119448274d0dd8
daengdaengLee <gunho1020@gmail.com>
2019-05-22 12:37:13 +0900

    ENH: Chapter 16 Section 01 Exam 02

---
+++ chap16/src/sec01/exam02/Student.java
@@ -0,0 +1,19 @@
+package sec01.exam02;
+
+public class Student {
+  public String name;
+  public int score;
+
+  public Student(String name, int score) {
+    this.name = name;
+    this.score = score;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getScore() {
+    return score;
+  }
+}
